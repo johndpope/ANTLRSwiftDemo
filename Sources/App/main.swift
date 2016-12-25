370b89da191033af9107d15ca4cccfb66281cd2b
Ewan Mellor <commits@ewanmellor.org>
2016-12-24 23:58:25 -0800

    Replace the template code in main.swift with a simple input form.
    The initial response is a form with a single text box, and the form
    posts back whatever text was entered.  That text is then echoed back.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,26 @@
+import HTTP
+import URI
 import Vapor
+
 
 let drop = Droplet()
 
 drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+    return try drop.view.make("parser-input", [
+        "originalText": "",
+        "responseText": "",
+        ])
 }
 
-drop.resource("posts", PostController())
+drop.post { req in
+    guard let text = req.data["text"]?.string else {
+        throw Abort.badRequest
+    }
+
+    return try drop.view.make("parser-input", [
+        "originalText": text,
+        "responseText": text,
+        ])
+}
 
 drop.run()
