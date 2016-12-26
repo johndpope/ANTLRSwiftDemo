bfe57a329f5dd91bb666420ad6864783d21f40d5
Ewan Mellor <commits@ewanmellor.org>
2016-12-25 16:52:58 -0800

    Integrate the ANTLR parser into the web app.
    This passes the user's input to the auto-generated GolangLexer and
    GolangParser, and responds with the lisp-ish form of the parse tree
    from ParseTree.toStringTree.
    
    This changes the project file to include the auto-generated files.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,6 +1,16 @@
+import Antlr4
 import HTTP
 import URI
 import Vapor
+
+
+func parse(input: String) throws -> String {
+    let lexer = GolangLexer(ANTLRInputStream(input))
+    let tokens = CommonTokenStream(lexer)
+    let parser = try GolangParser(tokens)
+    let tree = try parser.sourceFile()
+    return tree.toStringTree(parser)
+}
 
 
 let drop = Droplet()
@@ -17,9 +27,11 @@ drop.post { req in
         throw Abort.badRequest
     }
 
+    let responseText = try parse(input: text)
+
     return try drop.view.make("parser-input", [
         "originalText": text,
-        "responseText": text,
+        "responseText": responseText,
         ])
 }
 
